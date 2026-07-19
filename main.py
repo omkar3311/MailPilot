c9f1a9d739a9bec0d369ec4b7f6102a14f793634
omkar3311 <omkarwaghmare3311@gmail.com>
2026-07-19 17:33:54 +0530

    dotenv & oauth2 creds

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from fastapi import FastAPI , Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from dotenv import load_dotenv
+from google.oauth2.credentials import Credentials
