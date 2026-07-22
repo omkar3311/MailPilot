9dfaae27f7f29b9f6db7453c6ec66b5f921bafca
omkar3311 <omkarwaghmare3311@gmail.com>
2026-07-22 19:16:05 +0530

    imports

---
+++ utility.py
@@ -0,0 +1,12 @@
+import os
+import json
+import base64
+from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
+from groq import Groq
+from dotenv import load_dotenv
+import os
+from email.mime.text import MIMEText
+from datetime import datetime, timedelta
+
+import re
