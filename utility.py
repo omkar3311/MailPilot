f908f77575df0eea81194cdae933de05e501818f
omkar3311 <omkarwaghmare3311@gmail.com>
2026-08-05 10:16:12 +0530

    start time

--- utility.py
+++ utility.py
@@ -427,6 +427,14 @@ def find_available_slots(
     start_hour = 9
     end_hour = 18
     for hour in range(start_hour, end_hour):
+        start = datetime(
+            date.year,
+            date.month,
+            date.day,
+            hour,
+            0
+        )
+
 
 
 def handle_meeting_request(calendar, client, email, result):
